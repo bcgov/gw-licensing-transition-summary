b35dc1c4e3f42192a78464954658de6b132823d9
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2017-06-15 16:04:41 -0700

    clean file

--- 02_clean.R
+++ 02_clean.R
@@ -10,4 +10,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
+library(dplyr) # data munging
+
+## Load raw data if not already in local repository
+if (!exists("transition_lic_raw")) load("tmp/trans_gwlic_raw.RData")
+
+## clean transition_lic_raw 
+
+## Only keep columns in transition_lic necessary for data summaries
+keep_col <- c("AuthorizationType", "ApplicationDate", "StatusDescription", "VFCBCOffice")
+
+transition_lic <- transition_lic_raw %>% 
+  select(one_of(keep_col))
+
+
+## Create tmp folder if not already there and store clean data in local repository
+if (!exists("tmp")) dir.create("tmp", showWarnings = FALSE)
+save(transition_lic, file = "tmp/trans_gwlic_clean.RData")
 

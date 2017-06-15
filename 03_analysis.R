f6e971bb44ff92d54dfeecaa2d9a4215fd6ca047
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2017-06-15 16:04:51 -0700

    summary file

--- 03_analysis.R
+++ 03_analysis.R
@@ -10,4 +10,28 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
+library(dplyr) # data munging
+library(envreportutils) # order_df
 
+## Load clean data if not already in local repository
+if (!exists("transition_lic")) load("tmp/trans_gwlic_clean.RData")
+
+## make a total applications with estimated total dataframe
+tot_ta <- length(transition_lic$AuthorizationType)
+est_ta <- 20000
+remaining <- est_ta-tot_ta
+cat <- c("Estimated\nOutstanding", "Current\nNumber")
+val <- c(remaining, tot_ta)
+est.df <- data.frame(cat, val)
+
+est.df<- order_df(est.df, target_col = "cat", value_col = "val", fun = max, desc = TRUE)
+
+## number of licenses by application category
+ta_type <- transition_lic %>% 
+  group_by(StatusDescription) %>% 
+  summarise(number = length(StatusDescription))
+
+
+## Create tmp folder if not already there and store clean data in local repository
+if (!exists("tmp")) dir.create("tmp", showWarnings = FALSE)
+save(tot_ta, ta_type, est.df,  file = "tmp/trans_gwlic_summaries.RData")

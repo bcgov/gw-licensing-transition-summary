fc867035fcbf8a9e965780baa525b7893b7d5a0c
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2017-06-16 14:34:42 -0700

    summarise lic data for plotting

--- 03_analysis.R
+++ 03_analysis.R
@@ -13,10 +13,12 @@
 library(dplyr) # data munging
 library(envreportutils) # order_df
 
+
 ## Load clean data if not already in local repository
 if (!exists("transition_app")) load("tmp/trans_gwlic_clean.RData")
 
-## make a total applications with estimated total dataframe
+
+## make a 'total transition applications with estimated transitions' dataframe
 tot_ta <- length(transition_app$AuthorizationType)
 est_ta <- 20000
 remaining <- est_ta-tot_ta
@@ -27,7 +29,7 @@ est.df <- data.frame(cat, val)
 est.df<- order_df(est.df, target_col = "cat", value_col = "val", fun = max, desc = TRUE)
 
 
-## number of licenses by application category
+## number of applications by application category
 ta_type <- transition_app %>% 
   group_by(StatusDescription) %>% 
   summarise(number = length(StatusDescription))
@@ -39,6 +41,30 @@ cat.order <- c("Accepted", "Under Review", "Pending", "Submitted", "Pre-Submitta
 ## reordering the categories for plotting
 ta_type$StatusDescription <- factor(ta_type$StatusDescription, levels = cat.order)
 
+
+## number of licenses by status category
+tl_status <- transition_lic %>% 
+  group_by(JobStatus) %>% 
+  summarise(number = length(JobStatus))
+
+## Change 'Grant' to 'Granted'
+tl_status$JobStatus[tl_status$JobStatus == "Grant"] <- "Granted"
+
+## Subset for plotting only 2 categories
+tl_in_progress <- tl_status %>%
+  filter(JobStatus == "In Progress" | JobStatus == "Granted")
+
+## order tl_in_progress dataframe & change JobStatus Name
+tl_in_progress<- order_df(tl_in_progress, target_col = "JobStatus", value_col = "number", fun = max, desc = TRUE)
+
+
+## calculate number of parked and abandoned 
+tl_parked <- tl_status$number[tl_status$JobStatus == "Parked"]
+tl_abandon <- tl_status$number[tl_status$JobStatus == "Abandoned"]
+
+
 ## Create tmp folder if not already there and store clean data in local repository
 if (!exists("tmp")) dir.create("tmp", showWarnings = FALSE)
-save(tot_ta, ta_type, est.df, cat.order,  file = "tmp/trans_gwlic_summaries.RData")
+
+save(tot_ta, ta_type, est.df, cat.order, tl_in_progress,
+     tl_parked, tl_abandon, file = "tmp/trans_gwlic_summaries.RData")

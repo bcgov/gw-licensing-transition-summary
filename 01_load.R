d134eb49951d0db4853a2b63c76049d4084b8bee
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2017-07-07 14:29:49 -0700

    add projected app data

--- 01_load.R
+++ 01_load.R
@@ -13,31 +13,39 @@
 
 library(readxl) # MS Excel data import 
 
+
+## Import the projected number of transition Existing Use Groundwater License Applications
+projected_app <- read_excel("Z:/water/groundwater/licensing/transition/Projected_GW_Transition_Licenses_ENVWaterBranch.xlsx")
+
+
 ## Import the APPLICATION data for Existing Use Groundwater License Applications
 ## Data source is a MS Excel file export from 'FCBC Application Search' 
 ## provided by email from FLNRO staff (generated on demand)
 ## Data license: Access Only http://www2.gov.bc.ca/gov/content/home/copyright 
-
 transition_app_raw <- read_excel("Z:/water/groundwater/licensing/transition/Applications Summary.xlsx")
+## Data as of date
 app_date <- "June 30th 2017"
+
 
 ## Import the LICENSING data for Existing Use Groundwater Incoming Licenses
 ## Data source is a MS Excel file report/export from
 ## 'Incoming Groundwater Licence Application Tracking Report' through
 ## Virtual FrontCounter BC E-licensing Home, generated manually by ENV staff
 ## Data license: Access Only http://www2.gov.bc.ca/gov/content/home/copyright 
-
 transition_lic_raw <- read_excel("Z:/water/groundwater/licensing/transition/ApplicationPurposeUseSearch Jun 19 2017.xlsx")
+## Data as of date
 lic_date <- "June 19th 2017"
+
 
 ## Import the PROCESSING TIME data for Existing Use Groundwater Incoming Licensing
 ## Data source is a MS Excel file export from FCBC's 
 ## 'FrontCounterBC ATS system application processing time report' provided by
 ## email from FCBC FLNRO staff (generated monthly)
 ## Data license: Access Only http://www2.gov.bc.ca/gov/content/home/copyright 
-
 transition_processing_time_raw <- read_excel("Z:/water/groundwater/licensing/transition/GroundWater-June2017.xlsx")
+## Data as of date
 proctime_date <- "June 30th 2017"
+
 
 ## Create tmp folder if not already there and store raw data in local repository
 if (!exists("tmp")) dir.create("tmp", showWarnings = FALSE)

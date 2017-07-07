8351d88b66e7a470df38542bcb4e86bffeaf3a49
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2017-07-07 14:09:58 -0700

    update 3 data sources

--- 01_load.R
+++ 01_load.R
@@ -13,35 +13,34 @@
 
 library(readxl) # MS Excel data import 
 
-## Import the data for Existing Use Groundwater License Applications (CSV)
-## Data source is a manual export (MS Excel file) from 'FCBC Application Search' via
-## email from FCBC staff (generated monthly)
+## Import the APPLICATION data for Existing Use Groundwater License Applications
+## Data source is a MS Excel file export from 'FCBC Application Search' 
+## provided by email from FLNRO staff (generated on demand)
 ## Data license: Access Only http://www2.gov.bc.ca/gov/content/home/copyright 
 
 transition_app_raw <- read_excel("Z:/water/groundwater/licensing/transition/Applications Summary.xlsx")
+app_date <- "June 30th 2017"
 
-
-## Import the data for Existing Use Groundwater Incoming Licenses  (CSV)
-## Data source is a manual export (MS Excel file) from 
-## 'Incoming Groundwater Licence Application Tracking Report' via
-## Virtual FrontCounter BC E-licensing Home
+## Import the LICENSING data for Existing Use Groundwater Incoming Licenses
+## Data source is a MS Excel file report/export from
+## 'Incoming Groundwater Licence Application Tracking Report' through
+## Virtual FrontCounter BC E-licensing Home, generated manually by ENV staff
 ## Data license: Access Only http://www2.gov.bc.ca/gov/content/home/copyright 
 
 transition_lic_raw <- read_excel("Z:/water/groundwater/licensing/transition/ApplicationPurposeUseSearch Jun 19 2017.xlsx")
+lic_date <- "June 19th 2017"
 
-
-## Import the data for Existing Use Groundwater Incoming License Processing Time (CSV)
-## Data source is a manual export (MS Excel file) from 
-## 'FrontCounterBC ATS system application processing time report' via
-## email from FCBC staff (generated monthly)
+## Import the PROCESSING TIME data for Existing Use Groundwater Incoming Licensing
+## Data source is a MS Excel file export from FCBC's 
+## 'FrontCounterBC ATS system application processing time report' provided by
+## email from FCBC FLNRO staff (generated monthly)
 ## Data license: Access Only http://www2.gov.bc.ca/gov/content/home/copyright 
 
-transition_processing_time_raw <- read_excel("Z:/water/groundwater/licensing/transition/Groundwater Data until May 30, 2017 - run on June 5th .xlsx")
-
-datadate <- "June 19th 2017"
+transition_processing_time_raw <- read_excel("Z:/water/groundwater/licensing/transition/GroundWater-June2017.xlsx")
+proctime_date <- "June 30th 2017"
 
 ## Create tmp folder if not already there and store raw data in local repository
 if (!exists("tmp")) dir.create("tmp", showWarnings = FALSE)
-save(transition_app_raw, transition_lic_raw, datadate,
+save(transition_app_raw, transition_lic_raw, app_date, lic_date, proctime_date,
      transition_processing_time_raw, file = "tmp/trans_gwlic_raw.RData")
 

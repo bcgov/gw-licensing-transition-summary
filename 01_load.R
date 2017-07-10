145e92f852048b48c01ee60f57704193c49e25ca
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2017-07-10 14:01:02 -0700

    update comments; add 4th input file

--- 01_load.R
+++ 01_load.R
@@ -19,18 +19,24 @@ projected_app_raw <- read_excel("Z:/water/groundwater/licensing/transition/Proje
 
 
 ## Import the APPLICATION data for Existing Use Groundwater License Applications
-## Data source is a MS Excel file export from 'FCBC Application Search' 
-## provided by email from FLNRO staff (generated on demand)
+## Data source is a MS Excel file export from 'Application Search' sourced from vFCBC E-Licensing Home
 ## Data license: Access Only http://www2.gov.bc.ca/gov/content/home/copyright 
-transition_app_raw <- read_excel("Z:/water/groundwater/licensing/transition/Applications Summary.xlsx")
+transition_app_raw <- read_excel("Z:/water/groundwater/licensing/transition/ApplicationSearch_eASP.xlsx")
 ## Data 'as of' date
-app_date <- "June 30th 2017"
+app_date <- "July 4th 2017"
+
+
+# Import the APPLICATION data (version 2) for Existing Use Groundwater License Applications
+# Data source is a MS Excel file provided by email from FLNRO staff (generated on demand)
+# Data license: Access Only http://www2.gov.bc.ca/gov/content/home/copyright
+transition_appv2_raw <- read_excel("Z:/water/groundwater/licensing/transition/Applications Summary.xlsx")
+## Data 'as of' date
+appv2_date <- "June 30th 2017"
 
 
 ## Import the LICENSING data for Existing Use Groundwater Incoming Licenses
-## Data source is a MS Excel file report/export from
-## 'Incoming Groundwater Licence Application Tracking Report' through
-## Virtual FrontCounter BC E-licensing Home, generated manually by ENV staff
+## Data source is a MS Excel file report/export from 'Incoming Groundwater Licence Application Tracking Report'
+##  through E-licensing Home, generated manually by ENV staff
 ## Data license: Access Only http://www2.gov.bc.ca/gov/content/home/copyright 
 transition_lic_raw <- read_excel("Z:/water/groundwater/licensing/transition/ApplicationPurposeUseSearch.xlsx")
 ## Data 'as of' date
@@ -49,7 +55,7 @@ proctime_date <- "June 30th 2017"
 
 ## Create tmp folder if not already there and store raw data in local repository
 if (!exists("tmp")) dir.create("tmp", showWarnings = FALSE)
-save(projected_app_raw, transition_app_raw, transition_lic_raw,
-     processing_time_raw, app_date, lic_date, proctime_date,
+save(projected_app_raw, transition_app_raw, transition_appv2_raw, transition_lic_raw,
+     processing_time_raw, app_date, appv2_date, lic_date, proctime_date, 
      file = "tmp/trans_gwlic_raw.RData")
 

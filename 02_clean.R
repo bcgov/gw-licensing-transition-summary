cc2c1466869cb0cd106a7f345c40c3b203145815
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2017-07-11 11:09:55 -0700

    add regions file

--- 02_clean.R
+++ 02_clean.R
@@ -44,6 +44,11 @@ office_regions <- processing_time_raw %>%
   summarise(n = n()) %>% 
   select(-(n))
 
+regions <- office_regions %>% 
+  group_by(nrs_region) %>% 
+  summarise(n = n()) %>% 
+  select(-(n))
+
 ## Merge nrs_regions into transition_all df
 transition_app <- merge(transition_app, office_regions, by = "VFCBCOffice", all.x = TRUE)
   
@@ -78,5 +83,6 @@ processing_time <- filter(processing_time,
 ## Create tmp folder if not already there and store clean data in local repository
 if (!exists("tmp")) dir.create("tmp", showWarnings = FALSE)
 save(projected_app, transition_app, transition_lic, processing_time, 
-      app_date, lic_date, proctime_date, file = "tmp/trans_gwlic_clean.RData")
+      app_date, lic_date, proctime_date, office_regions,
+     regions, file = "tmp/trans_gwlic_clean.RData")
 

1fb3eef6d74f135dc0654ce4345a1ce071aafa92
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2017-11-08 15:07:02 -0800

    update purpose use & region plots

--- 03_analysis.R
+++ 03_analysis.R
@@ -42,13 +42,13 @@ ta_type <- virtual_clean %>%
   count(Job_Status) 
 
 ## arranging the order of the categories to be plotted
-cat.order <- c("Under Review", "Submitted & Pre-Review Steps", "Cancelled & Not Accepted")
+vir.order <- c("Under Review", "Submitted & Pre-Review Steps", "Cancelled & Not Accepted")
 
 ## reordering the categories for plotting
-ta_type$Job_Status <- factor(ta_type$Job_Status, levels = cat.order)
+ta_type$Job_Status <- factor(ta_type$Job_Status, levels = vir.order)
 
 ## set colour palette
-colours <- c("Accepted" = "#e41a1c",
+virtual.colours <- c("Accepted" = "#e41a1c",
              "Under Review" = "#377eb8",
              "Submitted & Pre-Review Steps" = "#4daf4a",
              "Cancelled & Not Accepted" = "#a65628")
@@ -58,7 +58,7 @@ ta_type_plot <- ggplot(ta_type, aes(1, y = n, fill = Job_Status)) +
   geom_col(alpha = 0.7) +
   geom_text(aes(label = n), position = position_stack(vjust = 0.5), size = 2) +
   labs(title = "Status of FrontCounter BC Transition\nApplication Intake Process") +
-  scale_fill_manual(values = colours, name = NULL, breaks = rev(levels(ta_type$Job_Status))) +
+  scale_fill_manual(values = virtual.colours, name = NULL, breaks = rev(levels(ta_type$Job_Status))) +
   xlab(NULL) +
   ylab(NULL) +
   theme_soe() +
@@ -79,6 +79,8 @@ plot(ta_type_plot)
 
 ## elic data summaries
 
+## @knitr elic_status
+
 ## number of e-licenses by status category with duplicate rows removed
 tl_status <- elic_clean %>% 
   group_by(JobStatus) %>% 
@@ -88,14 +90,13 @@ tl_status <- elic_clean %>%
 tl_status$JobStatus[tl_status$JobStatus == "Grant"] <- "Decision"
 
 ## arranging the order of the categories to be plotted
-cat.order3 <- c("Decision", "In Progress", "Parked", "Abandoned")
+elic.order <- c("Decision", "In Progress", "Parked", "Abandoned")
 
 ## reordering the categories for plotting
-tl_status$JobStatus <- factor(tl_status$JobStatus, levels = cat.order3)
-
+tl_status$JobStatus <- factor(tl_status$JobStatus, levels = elic.order)
 
 ## colour palette
-colr2 <- c("Abandoned" = "#a65628",
+elic.colour <- c("Abandoned" = "#a65628",
            "Parked" = "#4daf4a",
            "In Progress" = "#377eb8",
            "Decision" = "#e41a1c")
@@ -105,7 +106,7 @@ tl_status_plot <- ggplot(tl_status, aes(1, y = number, fill = JobStatus)) +
   geom_col(alpha = 0.7) +
   geom_text(aes(label = number), position = position_stack(vjust = 0.5), size = 2) +
   labs(title = "Status of FLNRO Adjudication: Transition Applications") +
-  scale_fill_manual(values = colr2, name = NULL,
+  scale_fill_manual(values = elic.colour, name = NULL,
                     breaks = rev(levels(tl_status$JobStatus))) +
   xlab(NULL) +
   ylab(NULL) +
@@ -123,6 +124,91 @@ tl_status_plot <- ggplot(tl_status, aes(1, y = number, fill = JobStatus)) +
   guides(fill = guide_legend(nrow = 1))
 
 plot(tl_status_plot)
+
+## @knitr elic_regions
+
+## Number applications and predicted number by Region
+tl_region <- elic_clean %>%
+  group_by(nrs_region) %>%
+  summarise(accepted = n() , decision = sum(JobStatus == "Grant")) %>%
+  merge(projected_app_clean, by = "nrs_region", all.y=TRUE) %>%
+  gather(type, value, -nrs_region) %>%
+  mutate(value = ifelse(is.na(value), 0, value))
+
+## arranging the order of the categories to be plotted
+elic.region.order <- c("projected", "accepted", "decision")
+
+## reordering the categories for plotting
+tl_region$type <- factor(tl_region$type, levels = elic.region.order)
+
+## order df for plotting
+tl_region<- order_df(tl_region, target_col = "nrs_region", value_col = "value", fun = max, desc = TRUE)
+
+## colour paletts
+elic.region.colours <- c("projected" = "#999999",
+           "accepted" = "#377eb8",
+           "decision" = "#e41a1c")
+
+new_lab <- c("projected" = "Projected",
+             "decision" = "Decision",
+             "accepted" = "Accepted")
+
+app_regions_plot <- ggplot(data = tl_region, aes(x = nrs_region, y = value, fill = type)) +
+  geom_bar(stat="identity", position = "dodge", alpha = 0.7) +
+  geom_text(aes(label = value), position = position_dodge(.9),  vjust = -.5, size = 2) +
+  labs(title = "Accepted & Granted Transition Applications Compared with\nProjected Numbers By NRS Region") +
+  scale_fill_manual(values = elic.region.colours, name=NULL,
+                    labels=new_lab) +
+  xlab(NULL) +
+  ylab("Number of Applications") +
+  theme_soe() +
+  scale_y_continuous(expand=c(0, 0), limits = c(0, 5200), breaks = seq(0, 5200, 1000), labels = scales::comma) +
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 8)) +
+  theme(panel.grid.major.x = element_blank(),
+        axis.title.y = element_text(size=10),
+        axis.text = element_text(size=10),
+        plot.title = element_text(size = 12, hjust = 0.5, face = "bold"),
+        plot.margin = unit(c(5,5,5,5),"mm"),
+        legend.text = element_text(size=10),
+        legend.position = c(.75,.76),
+        legend.background = element_rect(fill = "transparent"))
+
+plot(app_regions_plot)
+
+
+## number of elic licenses by purpose use category, includes duplicate IDs
+tl_purpose <- elic_clean_dup %>% 
+  group_by(PurposeUse) %>% 
+  summarise(number = length(PurposeUse)) %>% 
+  mutate(perc_tot = paste0(round((number/sum(number)*100), digits = 0),"%"))
+
+tl_purpose <- order_df(tl_purpose, target_col = "PurposeUse", value_col = "number", fun = max, desc = TRUE)
+
+## @knitr water_use
+
+## bar chart of Water Use Purposes for incoming E-licence applications
+
+tl_use_plot <- ggplot(tl_purpose, aes(x = PurposeUse, y = number)) +
+  geom_col(alpha = 0.7, fill = "#377eb8") +
+  geom_text(aes(label = perc_tot), vjust = .2, hjust = -.2, size = 3) +
+  labs(title = "Incoming Transition Licences by Water Use Purpose",
+       caption = "\n**Note: Some licences have more than one water use purpose") +
+  xlab(NULL) +
+  ylab("Number of Incoming Licences") +
+  theme_soe() +
+  coord_flip() +
+  scale_y_continuous(expand=c(0, 0), limits = c(0, max(tl_purpose$number) + max(tl_purpose$number/10))) +
+  theme(panel.grid.major.y = element_blank(),
+        axis.title = element_text(size=10),
+        axis.text.x = element_text(size=10),
+        axis.text.y = element_text(size=10),
+        plot.title = element_text(size = 12, hjust = 1.5, face = "bold"),
+        plot.margin = unit(c(5,5,5,5),"mm"),
+        legend.text = element_text(size=9))
+
+plot(tl_use_plot)
+
+
 
 
 
@@ -190,144 +276,8 @@ work_day_rate_to_achieve <- app_to_go/work_days_to_go
 
 
 
-## transition_lic summaries ##
-
-## Number applications and predicted number by Region
-tl_region <- transition_lic %>%
-  distinct(TrackingNumber, .keep_all = TRUE) %>% 
-  group_by(nrs_region) %>%
-  summarise(accepted = n() , granted = sum(JobStatus == "Grant")) %>%
-  merge(projected_app, by = "nrs_region", all.y=TRUE) %>%
-  gather(type, value, -nrs_region) %>%
-  mutate(value = ifelse(is.na(value), 0, value))
-
-## arranging the order of the categories to be plotted
-cat.order2 <- c("projected", "accepted", "granted")
-
-## reordering the categories for plotting
-tl_region$type <- factor(tl_region$type, levels = cat.order2)
-
-## order df for plotting
-tl_region<- order_df(tl_region, target_col = "nrs_region", value_col = "value", fun = max, desc = TRUE)
 
 
-
-## number of licenses by purpose use category, includes duplicate IDs
-tl_purpose <- transition_lic %>% 
-  group_by(PurposeUse) %>% 
-  summarise(number = length(PurposeUse)) %>% 
-  mutate(perc_tot = paste0(round((number/sum(number)*100), digits = 0),"%"))
-
-tl_purpose <- order_df(tl_purpose, target_col = "PurposeUse", value_col = "number", fun = max, desc = TRUE)
-
-
-
-## transition_time summaries ##
-
-## Number decisions and avg days and net avg days df
-time_region <- processing_time %>% 
-  filter(Authorization_Status == "Closed") %>% 
-  select(-Authorization_Status) %>% 
-  group_by(nrs_region, Authorization_Type) %>%
-  summarise(num_dec = n(),
-            avg_tot_time = round(mean(Total_Processing_Time), digits = 0),
-            avg_net_time = round(mean(Net_Processing_Time), digits = 0),
-            diff_time = round(mean(Total_Processing_Time-Net_Processing_Time), digits = 0)) %>%
-  ungroup() %>% 
-  gather(measure, value, -nrs_region, -Authorization_Type, -num_dec) %>% 
-  complete(nrs_region, Authorization_Type, measure, fill = list(value = 0)) %>% 
-  mutate(num_dec = ifelse(is.na(num_dec), 0, num_dec)) 
-
-
-## Throughput rates for recieved, accepted and decisions using processing time report
-
-## calculate the num recieved applications per day
-proc_time_rec <- processing_time %>%
-  filter(Authorization_Type == "Existing Groundwater Licence") %>% #  Authorization_Status == "Closed"
-  group_by(Received_Date) %>%
-  summarise(recperday = n()) %>%
-  mutate(reccumsum =  cumsum(recperday), Date = Received_Date)
-
-## Calculate received rate to-date starting March 2016
-recsum <- sum(proc_time_rec$recperday)
-recnumdays <- as.integer(difftime(as.POSIXct(max(proc_time_rec$Received_Date)), as.POSIXct(min(proc_time_rec$Received_Date)), units="days"))
-rec_rate <- recsum/recnumdays
-
-## calculate the num accepted applications per day
-proc_time_acc <- processing_time %>%
-  filter(Authorization_Type == "Existing Groundwater Licence", !is.na(Accepted_Date)) %>% #  Authorization_Status == "Closed"
-  group_by(Accepted_Date) %>%
-  summarise(accperday = n()) %>% 
-  mutate(acccumsum =  cumsum(accperday), Date = Accepted_Date)
-
-## Calculate received rate to-date starting March 2016
-accsum <- sum(proc_time_acc$accperday)
-accnumdays <- as.integer(difftime(as.POSIXct(max(proc_time_acc$Accepted_Date)), as.POSIXct(min(proc_time_acc$Accepted_Date)), units="days"))
-acc_rate <- accsum/accnumdays
-
-## calculate the num decisions per day
-proc_time_dec <- processing_time %>%
-  filter(Authorization_Type == "Existing Groundwater Licence", !is.na(`Granted/Offered_Date`)) %>% #  Authorization_Status == "Closed"
-  group_by(`Granted/Offered_Date`) %>%
-  summarise(decperday = n()) %>% 
-  mutate(deccumsum =  cumsum(decperday), Date = `Granted/Offered_Date`)
-
-## Calculate decisions rate to-date starting March 2016
-decsum <- sum(proc_time_dec$decperday)
-decnumdays <- as.integer(difftime(as.POSIXct(max(proc_time_dec$Date)), as.POSIXct(min(proc_time_dec$Date)), units="days"))
-dec_rate <- decsum/decnumdays
-
-## Merge 3 dfs and tidy df
-stage_rates <- proc_time_rec %>% 
-  left_join(proc_time_acc, by = "Date") %>% 
-  left_join(proc_time_dec, by = "Date") %>% 
-  select(c(Date, reccumsum, acccumsum, deccumsum)) %>% 
-  gather(measure, value, -Date) 
-
-## Individual lines for each transition licence 
-ind_proc_time_trans <- processing_time %>%
-  filter(Authorization_Type == "Existing Groundwater Licence") %>% 
-  rename(Decision_Date = `Granted/Offered_Date`) 
-
-pt_trans_tot <- ind_proc_time_trans %>% 
-  group_by(Authorization_Status) %>% 
-  summarise(Totals = n())
-
-ind_proc_time_trans <- rownames_to_column(ind_proc_time_trans, "ID")
-
-ind_proc_time_trans <- ind_proc_time_trans %>% 
-  mutate(Received = 0, Accepted = difftime(as.POSIXct(ind_proc_time_trans$Accepted_Date),
-                                             as.POSIXct(ind_proc_time_trans$Received_Date),
-         units="days"),
-         Decision = difftime(as.POSIXct(ind_proc_time_trans$Decision_Date),
-                             as.POSIXct(ind_proc_time_trans$Received_Date),
-                             units="days")) %>% 
-  select(ID, nrs_region, Authorization_Status, Received, Accepted, Decision) %>% 
-  gather(stage, days, -ID, -nrs_region, -Authorization_Status) 
-
-
-
-
-## Individual lines for each NEW licence 
-ind_proc_time_new <- processing_time %>%
-  filter(Authorization_Type == "New Groundwater Licence") %>% 
-  rename(Decision_Date = `Granted/Offered_Date`) 
-
-pt_new_tot <- ind_proc_time_new %>% 
-  group_by(Authorization_Status) %>% 
-  summarise(Totals = n())
-
-ind_proc_time_new <- rownames_to_column(ind_proc_time_new, "ID")
-
-ind_proc_time_new <- ind_proc_time_new %>% 
-  mutate(Received = 0, Accepted = difftime(as.POSIXct(ind_proc_time_new$Accepted_Date),
-                                           as.POSIXct(ind_proc_time_new$Received_Date),
-                                           units="days"),
-         Decision = difftime(as.POSIXct(ind_proc_time_new$Decision_Date),
-                             as.POSIXct(ind_proc_time_new$Received_Date),
-                             units="days")) %>% 
-  select(ID, nrs_region, Authorization_Status, Received, Accepted, Decision) %>% 
-  gather(stage, days, -ID, -nrs_region, -Authorization_Status) 
 
 
 ## Create tmp folder if not already there and store clean data in local repository

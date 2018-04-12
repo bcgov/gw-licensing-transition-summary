ace4081da4d884beaaa21decba3e9def40def06a
Jon Goetz <JGOETZ@GOVBC>
2018-04-12 10:06:27 -0700

    added officer:: powerpoint presentation to the run_all.R script (will consider integration with other scripts)

--- run_all.R
+++ run_all.R
@@ -21,4 +21,23 @@ rmarkdown::render("summary.Rmd",
                   output_file = outfile)
 
 
+## Create an additional PowerPoint file, if necessary
 
+library(officer)
+doc <- read_pptx() %>% 
+  add_slide(layout = "Title Slide", master = "Office Theme") %>% 
+  ph_with_text(type = "ctrTitle", str = "Status of Transitioning Groundwater User Water Licence Applications") %>%
+  ph_with_text(type = "subTitle", str = paste0("Data provided by FLNRO Water Management Branch on ", ddate)) %>%
+  add_slide(layout = "Title and Content", master = "Office Theme") %>% 
+  ph_with_gg_at(value = tl_rate_plot, width = 9, height = 5.5, left = 0.5, top = 1) %>% 
+  add_slide(layout = "Title and Content", master = "Office Theme") %>% 
+  ph_with_gg_at(value = tot_est_plot, width = 9, height = 3.8, left = 0.5, top = 2) %>% 
+  add_slide(layout = "Title and Content", master = "Office Theme") %>% 
+  ph_with_gg_at(value = ta_type_plot, width = 9, height = 3.8, left = 0.5, top = 2) %>% 
+  add_slide(layout = "Title and Content", master = "Office Theme") %>% 
+  ph_with_gg_at(value = tl_status_plot, width = 9, height = 3.8, left = 0.5, top = 2) %>% 
+  add_slide(layout = "Title and Content", master = "Office Theme") %>% 
+  ph_with_gg_at(value = app_regions_plot, width = 9, height = 7, left = 0.5, top = 0.1) %>% 
+  add_slide(layout = "Title and Content", master = "Office Theme") %>% 
+  ph_with_gg_at(value = tl_use_plot, width = 9, height = 7, left = 0.5, top = 0.1)
+print(doc, target = paste0("ENV_GW_Licensing_Transition_Summary_asof_", ddate, ".pptx"))

99a71bea30d1faa9d5c314ef453520b419643645
jongoetz <jon.goetz@gov.bc.ca>
2018-08-14 14:25:24 -0700

    renamed columns to match source data

--- 02_clean.R
+++ 02_clean.R
@@ -25,7 +25,7 @@ projected_app_clean <- projected_app_raw %>%
   mutate(Projected = round(Projected, digits = 0))
 
 ## Cross walking the two data frames:
-## virtual_raw$Water_Tracking_Number == elic_raw$JobID
+## virtual_raw$Water_Tracking_Number == elic_raw$JobNumber
 ## virtual_raw$Date_Accepted == elic_raw$CreatedDate
 
 ## Clean virtual_raw
@@ -37,12 +37,12 @@ colnames(virtual_raw) <- gsub("\\s+", "_", colnames(virtual_raw))
 ## filter out new licence applications and
 ## filter out duplicate licence using VCFBC_Tracking_Number
 keep_col_virtual <- c("Application_Type", "Date_Submitted", "VFCBC_Tracking_Number",
-                      "Water_Tracking_Number",  "Job_Status")
+                      "Water_Tracking_Number",  "Job_status")
 
 virtual_clean <- virtual_raw %>%
   select(one_of(keep_col_virtual)) %>%
   filter(Application_Type == "Existing Use Groundwater Application") %>% 
-  distinct(Application_Type, Date_Submitted, VFCBC_Tracking_Number, Water_Tracking_Number, Job_Status, .keepall = TRUE)
+  distinct(Application_Type, Date_Submitted, VFCBC_Tracking_Number, Water_Tracking_Number, Job_status, .keepall = TRUE)
 
 ## Fix date formatting from character to Date
 virtual_clean$Date_Submitted <- as.Date(virtual_clean$Date_Submitted, format = "%m/%d/%Y")
@@ -50,19 +50,19 @@ virtual_clean$Date_Submitted <- as.Date(virtual_clean$Date_Submitted, format = "
 
 ## Clean e-Lic_raw
 ## Only keep columns in transition_lic necessary for plots, and filter out new licence applications
-keep_col_elic <- c("JobID", "TrackingNumber", "ApplicationType", "NewExistingUse", "JobStatus", "Region",
+keep_col_elic <- c("JobNumber", "TrackingNumber", "ApplicationType", "NewExistingUse", "JobStatus", "Region",
                   "PurposeUse", "Volume1000m3y")
 
 elic_clean_dup <- elic_raw %>% 
   select(one_of(keep_col_elic)) %>% 
-  filter(NewExistingUse == "Existing Use")
+  filter(NewExistingUse == "Existing Use (Transitioning GW)")
 
 ## Filter our duplicate licence applications using TrackingNumber and columns,
 ## some plots not useful without those duplicates (e.g. water use types)
 ## Add NRS regions for comparing with Projected dataframe
 elic_clean <- elic_clean_dup %>% 
-  select(JobID, TrackingNumber, ApplicationType, NewExistingUse, JobStatus, Region) %>% 
-  distinct(JobID, TrackingNumber, ApplicationType, NewExistingUse, JobStatus, Region,
+  select(JobNumber, TrackingNumber, ApplicationType, NewExistingUse, JobStatus, Region) %>% 
+  distinct(JobNumber, TrackingNumber, ApplicationType, NewExistingUse, JobStatus, Region,
           .keep_all = TRUE) %>% 
   rename(`E-licence Regions` = Region) %>% 
   left_join(regions)
